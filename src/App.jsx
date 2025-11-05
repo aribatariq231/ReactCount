fee43698f67f7554442dd1b69b73a9322d257fb7
aribatariq231 <aribatariq231@gmail.com>
2025-11-05 23:18:54 +0500

    increment-decrement-percentage-weather

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
 import { useState } from "react";
+import axios from "axios";
 
 function App(){
   const[count,setCount] = useState(0)
+  const[weather, setWeather]=useState({})
   const[obtMarks, setObtMarks] = useState(904)
   const[totalMarks, setTotalMarks] = useState(1100)
   const[percentage, setPercentage] = useState(0)
+
+  // weather
+  const[cityName, setCityName] = useState()
+  const handleChange=(e)=>{
+    setCityName(e.target.value)
+  }
   
+  const handleSubmit= async(e)=>{
+    e.preventDefault()
+    // alert("Submit Successfully!")
+    try{
+      const res = await axios.get(`https:p2pclouds.up.railway.app/v1/learn/weather?city=${cityName}`)
+      setWeather(res.data)
+    }
+    catch(err){
+      alert("Error")
+    }
+  }
+  // console.log(cityName)
+
+  // increment
   function increment(){
     setCount(count+1)
   }
@@ -16,8 +38,24 @@ function App(){
     setPercentage((obtMarks/totalMarks)*100)
   }
 
-  
-  return(
+   return(
+  <>
+  <form onSubmit={handleSubmit}>
+    <div>
+      <label htmlFor="cityName">City Name:</label>
+    </div>
+    <div>
+      <input type="text" name="cityName" id="cityName" placeholder="Enter your city Name" onChange={handleChange} />
+    </div>
+    <button type="submit">Get</button>
+  </form>
+  <div>
+    <h1>{weather.location && weather.location.name}</h1>
+    <h1>{weather.location && weather.location.region}</h1>
+    <h1>{weather.current && weather.current.temp_c}</h1>
+  </div>
+
+  {/* increment and decrement */}
     <div>
       Count {count}
       <h1>Obtained Percentage : {percentage}</h1>
@@ -26,6 +64,7 @@ function App(){
       <button onClick={decrement}>Decrement</button>
       <button onClick={calculatePer}>Percentage</button>
     </div>
+  </>  
   )
 }
 export default App;
